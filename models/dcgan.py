a187ca99d8c91fe2c47dfdd19b680566429b974d
tuguldurio <ok.tuguldur@gmail.com>
2020-12-08 21:37:33 -0500

    generator

--- models/dcgan.py
+++ models/dcgan.py
@@ -2,16 +2,43 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+class Generator(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.deconv1 = nn.ConvTranspose2d( 10, 512, 4, 1, 0, bias=False)
+        self.bn1     = nn.BatchNorm2d(512)
+        self.deconv2 = nn.ConvTranspose2d(512, 256, 4, 2, 1, bias=False)
+        self.bn2     = nn.BatchNorm2d(256)
+        self.deconv3 = nn.ConvTranspose2d(256, 128, 4, 2, 1, bias=False)
+        self.bn3     = nn.BatchNorm2d(128)
+        self.deconv4 = nn.ConvTranspose2d(128,  64, 4, 2, 1, bias=False)
+        self.bn4     = nn.BatchNorm2d(64)
+        self.deconv5 = nn.ConvTranspose2d( 64,   1, 4, 2, 1, bias=False)
+
+    def forward(self, x):
+        x = F.relu(self.bn1(self.deconv1(x)))
+        x = F.relu(self.bn2(self.deconv2(x)))
+        x = F.relu(self.bn3(self.deconv3(x)))
+        x = F.relu(self.bn4(self.deconv4(x)))
+        x = torch.tanh(self.deconv5(x))
+        return x
+
+    def weight_init(self, mean, std):
+        for m in self._modules:
+            if isinstance(m, nn.Conv2d):
+                m.weight.data.normal_(mean, std)
+                m.bias.data.zero_()
+
 class Discriminator(nn.Module):
     def __init__(self):
         super().__init__()
         self.conv1 = nn.Conv2d(  1,  64, 4, 2, 1, bias=False)
         self.conv2 = nn.Conv2d( 64, 128, 4, 2, 1, bias=False)
-        self.bn2 = nn.BatchNorm2d(128)
+        self.bn2   = nn.BatchNorm2d(128)
         self.conv3 = nn.Conv2d(128, 256, 4, 2, 1, bias=False)
-        self.bn3 = nn.BatchNorm2d(256)
+        self.bn3   = nn.BatchNorm2d(256)
         self.conv4 = nn.Conv2d(256, 512, 4, 2, 1, bias=False)
-        self.bn4 = nn.BatchNorm2d(512)
+        self.bn4   = nn.BatchNorm2d(512)
         self.conv5 = nn.Conv2d(512,   1, 4, 1, 0, bias=False)
 
     def forward(self, x):
@@ -35,7 +62,15 @@ def test():
     print(D)
     x = torch.rand(4, 1, 64, 64)
     y = D(x)
-    print(y)
+    print(y.shape)
+
+    G = Generator()
+    G.weight_init(0, 0.2)
+    print(G)
+    x = torch.rand(4, 10, 1, 1)
+    y = G(x)
+    print(y.shape)
+
 
 if __name__ == '__main__':
     test()
