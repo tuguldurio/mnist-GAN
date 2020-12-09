3b3634161485a941dd831b48604695f412c9e57b
tuguldurio <ok.tuguldur@gmail.com>
2020-12-09 18:04:17 -0500

    correct model with 64pixel img

--- models/dcgan.py
+++ models/dcgan.py
@@ -3,17 +3,17 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 class Generator(nn.Module):
-    def __init__(self):
+    def __init__(self, d=128):
         super().__init__()
-        self.deconv1 = nn.ConvTranspose2d( 10, 512, 4, 1, 0, bias=False)
-        self.bn1     = nn.BatchNorm2d(512)
-        self.deconv2 = nn.ConvTranspose2d(512, 256, 4, 2, 1, bias=False)
-        self.bn2     = nn.BatchNorm2d(256)
-        self.deconv3 = nn.ConvTranspose2d(256, 128, 4, 2, 1, bias=False)
-        self.bn3     = nn.BatchNorm2d(128)
-        self.deconv4 = nn.ConvTranspose2d(128,  64, 4, 2, 1, bias=False)
-        self.bn4     = nn.BatchNorm2d(64)
-        self.deconv5 = nn.ConvTranspose2d( 64,   1, 4, 2, 1, bias=False)
+        self.deconv1 = nn.ConvTranspose2d(100, d*8, 4, 1, 0, bias=False)
+        self.bn1     = nn.BatchNorm2d(d*8)
+        self.deconv2 = nn.ConvTranspose2d(d*8, d*4, 4, 2, 1, bias=False)
+        self.bn2     = nn.BatchNorm2d(d*4)
+        self.deconv3 = nn.ConvTranspose2d(d*4, d*2, 4, 2, 1, bias=False)
+        self.bn3     = nn.BatchNorm2d(d*2)
+        self.deconv4 = nn.ConvTranspose2d(d*2, d, 4, 2, 1, bias=False)
+        self.bn4     = nn.BatchNorm2d(d)
+        self.deconv5 = nn.ConvTranspose2d(d, 1, 4, 2, 1, bias=False)
 
     def forward(self, x):
         x = F.relu(self.bn1(self.deconv1(x)))
@@ -30,16 +30,16 @@ class Generator(nn.Module):
                 m.bias.data.zero_()
 
 class Discriminator(nn.Module):
-    def __init__(self):
+    def __init__(self, d=128):
         super().__init__()
-        self.conv1 = nn.Conv2d(  1,  64, 4, 2, 1, bias=False)
-        self.conv2 = nn.Conv2d( 64, 128, 4, 2, 1, bias=False)
-        self.bn2   = nn.BatchNorm2d(128)
-        self.conv3 = nn.Conv2d(128, 256, 4, 2, 1, bias=False)
-        self.bn3   = nn.BatchNorm2d(256)
-        self.conv4 = nn.Conv2d(256, 512, 3, 2, 1, bias=False)
-        self.bn4   = nn.BatchNorm2d(512)
-        self.conv5 = nn.Conv2d(512,   1, 4, 1, 0, bias=False)
+        self.conv1 = nn.Conv2d(1, d, 4, 2, 1, bias=False)
+        self.conv2 = nn.Conv2d(d, d*2, 4, 2, 1, bias=False)
+        self.bn2   = nn.BatchNorm2d(d*2)
+        self.conv3 = nn.Conv2d(d*2, d*4, 4, 2, 1, bias=False)
+        self.bn3   = nn.BatchNorm2d(d*4)
+        self.conv4 = nn.Conv2d(d*4, d*8, 4, 2, 1, bias=False)
+        self.bn4   = nn.BatchNorm2d(d*8)
+        self.conv5 = nn.Conv2d(d*8, 1, 4, 1, 0, bias=False)
 
     def forward(self, x):
         x = F.leaky_relu(self.conv1(x), 0.2)
@@ -47,7 +47,7 @@ class Discriminator(nn.Module):
         x = F.leaky_relu(self.bn3(self.conv3(x)), 0.2)
         x = F.leaky_relu(self.bn4(self.conv4(x)), 0.2)
         x = torch.sigmoid(self.conv5(x))
-        return x
+        return x.view(-1, 1).squeeze(1)
 
     def weight_init(self, mean, std):
         for m in self._modules:
@@ -60,13 +60,13 @@ if __name__ == '__main__':
     D = Discriminator()
     D.weight_init(0, 0.2)
     print(D)
-    x = torch.rand(4, 1, 28, 28)
+    x = torch.rand(4, 1, 64, 64)
     y = D(x)
     print(y.shape)
 
     G = Generator()
     G.weight_init(0, 0.2)
     print(G)
-    x = torch.rand(4, 10, 1, 1)
+    x = torch.rand(4, 100, 64, 64)
     y = G(x)
     print(y.shape)
